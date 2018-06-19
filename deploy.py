fbec4bbe0ee1c3fbee02a371b0bd97f0523ea247
Shraeyas <shraeyassingh@gmail.com>
2018-06-19 23:14:21 +0530

    initial

--- deploy.py
+++ deploy.py
@@ -6,5 +6,16 @@ app = Flask (__name__)
 def homepage ():
     return render_template ('bot.html')
 
+@app.route ('/api/bot', methods = ['POST'])
+def say_name ():
+    json = request.get_json ()
+    class_, question, response, suggestions = FAQbot.chat (json['query'])
+
+    dict = {}
+    for i in range (0, len(suggestions)):
+        dict[str(i)] = (suggestions[i][0])
+
+    return jsonify (type = class_, ques = question, answer = response, suggest = dict)
+
 if __name__ == '__main__':
     app.run ()
