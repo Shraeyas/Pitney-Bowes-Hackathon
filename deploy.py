8219316a4c3d72fa072aa2144aadd056f2699fbf
Shraeyas <shraeyassingh@gmail.com>
2018-06-19 22:59:44 +0530

    Initial Comit

--- deploy.py
+++ deploy.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request, jsonify
-
+import FAQbot
 app = Flask (__name__)
 
 @app.route ('/')
 def homepage ():
-    return "Yes"
-    #return render_template ('bot.html')
+    return render_template ('bot.html')
 
 if __name__ == '__main__':
     app.run ()
